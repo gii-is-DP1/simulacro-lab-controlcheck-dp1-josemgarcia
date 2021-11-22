27aade3237185eecba12f834b63617ea9e784cde
josemgarcia <josemgarcia@us.es>
2021-11-22 11:51:04 +0100

    Tests

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -2,12 +2,23 @@ package org.springframework.samples.petclinic.product;
 
 import lombok.Getter;
 import lombok.Setter;
+import javax.persistence.Entity;
+import org.springframework.samples.petclinic.model.NamedEntity;
+import javax.validation.constraints.DecimalMin;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
+@Entity
+@Table(name="products")
+public class Product extends NamedEntity {
+    
+    @DecimalMin(value="0")
     double price;
+
+    @ManyToOne
+    @JoinColumn(name="type")
     ProductType productType;
 }
